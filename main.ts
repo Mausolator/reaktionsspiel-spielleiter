a21cec30e9553b715447d97dc6e9edada92b95e5
Mausolator <alexander.miarka@gmail.com>
2026-03-06 15:17:48 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-radio.onReceivedNumber(function (receivedNumber) {
-	
-})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Aktion = randint(0, 3)
     if (Aktion == 0) {
@@ -13,5 +10,29 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
         radio.sendNumber(3)
     }
 })
+radio.onReceivedString(function (receivedString) {
+    if (true) {
+        if (receivedString == "player") {
+            radio.sendString("1")
+        } else if (false) {
+        	
+        }
+    }
+})
+radio.onReceivedValue(function (name, value) {
+    if ("1" == name && value == Aktion) {
+        Punkte_S1 += 1
+    } else if ("2" == name && value == Aktion) {
+        Punkte_S2 += 1
+    }
+    if ("1" == name && value != Aktion) {
+        Punkte_S1 += Punkte_S1 - 1
+    }
+    if ("2" == name && value != Aktion) {
+        Punkte_S2 += Punkte_S2 - 1
+    }
+})
+let Punkte_S2 = 0
+let Punkte_S1 = 0
 let Aktion = 0
 radio.setGroup(88)
